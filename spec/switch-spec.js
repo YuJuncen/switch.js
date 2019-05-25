5d9825f201b56d5bbd678b705ead9373043a8896
hillium <maruruku@stu.csust.edu.cn>
2019-05-25 20:36:46 +0800

    fix a fetal error...

--- spec/switch-spec.js
+++ spec/switch-spec.js
@@ -1,20 +1,22 @@
 const match = require('../src/switch');
 
-it ('Should run when default called.', () => {
-    expect(match(null).default(1), 1);
-    expect(match('neko!').default(0), 0);
-    expect(match(1).default('koto!'), 'koto!');
-})
+describe('match function tests.', () => {
+    it ('Should run when default called.', () => {
+        expect(match(null).default(1)).toBe(1);
+        expect(match('neko!').default(0)).toBe(0);
+        expect(match(1).default('koto!')).toEqual('koto!');
+    })
 
-it ('Should match some case if there are.', () => {
-    expect(match(1)
-        .case(x => x > 0, 'x > 0')
-        .run(), 'x > 0');
-})
+    it ('Should match some case if there are.', () => {
+        expect(match(1)
+            .case(x => x > 0, 'x > 0')
+            .run()).toEqual('x > 0');
+    })
 
-it ('Should match some case even there are more cases.', () => {
-    expect(match(1)
-        .case(x => x > 0, 'x > 0')
-        .case(x => x < 0, 'x < 0')
-        .run(), 'x < 0')
+    it('Should match some case even there are more cases.', () => {
+        expect(match(1)
+            .case(x => x > 0, true)
+            .case(x => x < 0, false)
+            .run()).toBe(true);
+    })
 })
